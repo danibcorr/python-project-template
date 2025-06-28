c75926b3580d0738a4a863eb5eb694c2ad79505e
Daniel Bazo <77023868+danibcorr@users.noreply.github.com>
2025-06-28 13:19:40 +0200

    Test new actions releases

---
+++ src/version.py
@@ -0,0 +1,2 @@
+# Unique source of truth for the version number.
+__version__: str = "2.3.1"
---
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_module_name }}/__init__.py
@@ -0,0 +1,5 @@
+# Own modules
+from {{ cookiecutter.project_module_name }}.version import __version__
+
+# Define all names to be imported
+__all__: list[str] = ["__version__"]
