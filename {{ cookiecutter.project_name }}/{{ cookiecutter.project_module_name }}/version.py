02150ec6828330a38260a9365b4d059518c5d453
danibcorr <77023868+danibcorr@users.noreply.github.com>
2025-08-05 20:14:30 +0200

    Fix version format

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_module_name }}/version.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_module_name }}/version.py
@@ -6,4 +6,4 @@ Unique source of truth for the version number.
 import importlib.metadata
 
 
-__version__: str = importlib.metadata.version({{ cookiecutter.project_module_name }})
+__version__: str = importlib.metadata.version("{{ cookiecutter.project_module_name }}")
