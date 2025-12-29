96ac0af8f11d42ea88255961c15187c8b0612d97
danibcorr <77023868+danibcorr@users.noreply.github.com>
2025-12-29 15:57:34 +0100

    Changed hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,31 +1,19 @@
 import os
 import shutil
-
 from pathlib import Path
 
-current_path = Path(os.getcwd())
-parent_path = current_path.parent.absolute()
+project_dir = Path.cwd().parent / "{{ cookiecutter.project_name }}"
 
-def remove(filepath) -> None:
-    if os.path.isfile(filepath):
-        os.remove(filepath)
-    elif os.path.isdir(filepath):
-        shutil.rmtree(filepath)
-        
-cookiecutter_prompts_folder: dict[str, str] = {
-    'add_prompt_folder': 'prompt'
+def remove(path: Path) -> None:
+    if path.is_file():
+        path.unlink()
+    elif path.is_dir():
+        shutil.rmtree(path)
+
+folders = {
+    "prompt": "{{ cookiecutter.add_prompt_folder }}",
 }
 
-for cookiecutter_key, folder_name in folders_to_add.items():
-
-    cookiecutter_var: str = '{{cookiecutter.' + f'{cookiecutter_key}' + '}}'
-    add_folder: bool = cookiecutter_var == 'yes'
-    
-    if not add_folder:
-        folder_path = os.path.join(
-            parent_path, 
-            '{{cookiecutter.project_name}}', 
-            folder_name
-        )
-
-        remove(folder_path)
+for folder, enabled in folders.items():
+    if enabled != "yes":
+        remove(project_dir / folder)
